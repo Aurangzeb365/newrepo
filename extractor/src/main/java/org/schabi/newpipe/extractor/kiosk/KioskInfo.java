d292f011085b0cc8d44931ea63ae1f740a574132
Muhammad Aurangzeb <zaib365@yahoo.com>
2024-07-26 00:19:17 +0100

    1

--- extractor/src/main/java/org/schabi/newpipe/extractor/kiosk/KioskInfo.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/kiosk/KioskInfo.java
@@ -9,7 +9,7 @@ package org.schabi.newpipe.extractor.kiosk;
  * NewPipe Extractor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version...
+ * (at your option) any later version...g
  * NewPipe Extractor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
